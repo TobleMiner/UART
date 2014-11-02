1436e4c0c242cb0b5ed3b7d587ac932339f60e8a
Tobias Schramm <tobleminer@gmail.com>
2014-11-02 04:32:49 +0100

    Added stream-like uart_data_available() and uart_read() functions

--- src/uart.c
+++ src/uart.c
@@ -1,9 +1,32 @@
+/*
+Copyright (c) 2014 Tobias Schramm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include <avr/interrupt.h>
 
 #if UART_ENABLE_RX == TRUE
 volatile uint8_t uart_rx_ring[UART_BUFF_LEN_RX]; //Init buffer for uart-rx
 volatile uint8_t *uart_rx_curpos_ring = uart_rx_ring;
 volatile uint8_t *uart_rx_targpos_ring = uart_rx_ring;
+volatile uint32_t uart_rx_data_len = 0;
 #endif
 
 #if UART_ENABLE_TX == TRUE
@@ -31,7 +54,33 @@ void uart_init_rx(void)
 	UCSR0B |= (1<<RXCIE0) | (1<<RXEN0);
 }
 
-ISR(USART_RX_vect)
+uint32_t uart_data_available(void)
+{
+	return uart_rx_data_len;
+}
+
+uint32_t uart_read(uint8_t* data, uint32_t len)
+{
+	uint32_t tlen = 0;
+	uint8_t* tptr = data;
+	cli();
+	while(tlen < len && uart_rx_curpos_ring != uart_rx_targpos_ring)
+	{
+		*tptr = *uart_rx_curpos_ring;
+		tptr++;
+		tlen++;
+		uart_rx_curpos_ring++;
+		if(uart_rx_curpos_ring >= UART_BUFF_LEN_RX + uart_rx_ring) //Wrap pointer if necessary
+		{
+			uart_rx_curpos_ring = uart_rx_ring;
+		}
+	}
+	sei();
+	uart_rx_data_len -= tlen;
+	return tlen;
+}
+
+void uart_irq_rx(void)
 {
 	volatile uint8_t *tmpptr = uart_rx_targpos_ring;
 	*tmpptr = UDR0; //Read received byte to ringbuffer
@@ -41,7 +90,15 @@ ISR(USART_RX_vect)
 		tmpptr = uart_rx_ring;
 	}
 	uart_rx_targpos_ring = tmpptr;
+	uart_rx_data_len++;	
 }
+
+#if UART_IRQ_HOOK_RX != TRUE
+ISR(USART_RX_vect)
+{
+	uart_irq_rx();
+}
+#endif
 #endif
 
 #if UART_ENABLE_TX == TRUE
@@ -89,7 +146,7 @@ void uart_write_async(char* str)
 	UCSR0B |= (1<<TXCIE0); //uart0: Enable tx complete interrupt
 }
 
-ISR(USART_TX_vect)
+void uart_irq_tx(void)
 {
 	if(uart_tx_curpos_ring != uart_tx_targpos_ring)
 	{
@@ -102,7 +159,14 @@ ISR(USART_TX_vect)
 	}
 }
 
-ISR(USART_UDRE_vect)
+#if UART_IRQ_HOOK_TX != TRUE
+ISR(USART_TX_vect)
+{
+	uart_irq_tx();
+}
+#endif
+
+void uart_irq_udre(void)
 {
 	UDR0 = *uart_tx_curpos_ring; //Push first byte to UDR to start the transmission
 	uart_tx_curpos_ring++;
@@ -112,4 +176,11 @@ ISR(USART_UDRE_vect)
 	}
 	UCSR0B &= ~(1<<UDRIE0); //uart0: Disable uart data-register-empty interrupt
 }
+
+#if UART_IRQ_HOOK_UDRE != TRUE
+ISR(USART_UDRE_vect)
+{
+	uart_irq_udre();
+}
+#endif
 #endif
